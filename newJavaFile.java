800af07dd7f2b6ceeba22cc763dcc34ec30b08c9
Diego Andrés Barbosa Trujillo <da.barbosa10@uniandes.edu.co>
2022-06-05 18:51:22 +0200

    Add files via upload

---
+++ newJavaFile.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author diegobarbosa
+ */
+public class newJavaFile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        System.out.println("I love pizza");
+        System.out.println("It's really good");
+    }
+}
