5f63a20bc50ea8d62ef5949b085527c569a5f02f
Diego Andrés Barbosa Trujillo <da.barbosa10@uniandes.edu.co>
2022-06-06 19:16:57 +0200

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author diegobarbosa
+ */
+public class loops {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        int time=24;
+        if(time<12){
+            System.out.println("Good morning! ");
+        }
+        else if(time<18){
+            System.out.print("Good afternoon! ");
+        }
+        else{
+            System.out.print("Good evening! ");
+        }
+    double pii=3.1416;
+    int results=(pii==3.1416) ? 1 : 0 ;
+    System.out.print("The result is:" + results);
+    }
+    
+    
+   
+    
+    
+}
