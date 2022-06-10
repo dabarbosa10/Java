cc8a6ea3c842b7775c74424ad98fecdf1b89bace
Diego Andrés Barbosa Trujillo <da.barbosa10@uniandes.edu.co>
2022-06-10 18:35:00 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author diegobarbosa
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public void fullThrottle(){
+        System.out.println("The car is going as fast as it can!");
+    }
+    public void speed(int maxSpeed){
+        System.out.println("Max Speed is " + maxSpeed);
+    }
+    
+}
---
+++ Second.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author diegobarbosa
+ */
+public class Second {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        Main myCar=new Main();
+        myCar.fullThrottle();
+        myCar.speed(200);
+    }
+}
